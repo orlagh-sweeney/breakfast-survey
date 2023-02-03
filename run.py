9025951b3951d6bc9686a73d141071f00ea1338f
orlagh-sweeney <orlagh16@gmail.com>
2023-02-03 10:00:45 +0000

    Create welcome function to print welcome message

--- run.py
+++ run.py
@@ -12,8 +12,35 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('breakfast_survey')
 
-bkdata = SHEET.worksheet('bkdata')
 
-data = bkdata.get_all_values()
+def welcome():
+    """
+    Displays a welcome message and introduction to the survey
+    """
+    print("""\
+                             #        #                   #
+ ####                        #       ###                  #
+ #   #                       #  #    #                   ###
+ ####   # #     ##     ###   # #    ###     ###   ####    #
+ #   #  ## #   # ##   #  #   ###     #     #  #   ##      # #
+ #   #  #      ##     #  #   #  #    #     #  #     ##    # #
+ ####   #       ###    ####  #  #    #      ####  ####     #
 
-print(data)
+
+         ##
+        #  #
+         #     #  #   # #    #  #    ##    #  #
+          #    #  #   ## #   #  #   # ##   #  #
+        #  #   #  #   #       ##    ##     ## #
+         ##     ###   #       ##     ###     ##
+                                           #  #
+                                            ##
+    """)
+
+    print("""Welcome to the Breakfast Survey.\n
+    This survey analyses breakfast eating habits based on gender and age.\n
+    You can view the results, read a summary or add to the data by completing
+    the survey.""")
+
+
+welcome()
