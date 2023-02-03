3e05193c9552535bebd5032482326477fec06392
orlagh-sweeney <orlagh16@gmail.com>
2023-02-03 10:36:42 +0000

    Import prettytable and plotext libraries

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import plotext as plt
+from prettytable import PrettyTable
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -17,7 +19,8 @@ def welcome():
     """
     Displays a welcome message and introduction to the survey
     """
-    print("""\
+    print(
+        """\
                              #        #                   #
  ####                        #       ###                  #
  #   #                       #  #    #                   ###
@@ -35,12 +38,17 @@ def welcome():
          ##     ###   #       ##     ###     ##
                                            #  #
                                             ##
-    """)
+    """
+    )
 
-    print("""Welcome to the Breakfast Survey.\n
-    This survey analyses breakfast eating habits based on gender and age.\n
-    You can view the results, read a summary or add to the data by completing
-    the survey.""")
+    print(
+        """
+        Welcome to the Breakfast Survey.\n
+        This survey analyses breakfast eating habits based on gender and age.\n
+        You can view the results, read a summary or add to the data by
+        completing the survey.
+        """
+    )
 
 
 def main():
