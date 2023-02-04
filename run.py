ea149135b5ab7b9b994278a001c137c8227bb0e3
orlagh-sweeney <orlagh16@gmail.com>
2023-02-04 18:48:02 +0000

    Create calculate_gender_results function and calculate question 1

--- run.py
+++ run.py
@@ -22,48 +22,69 @@ df = pd.DataFrame(data, columns=headers)
 df.head()
 print(df)
 
-
-def welcome():
+def calculate_gender_results():
     """
-    Displays a welcome message and introduction to the survey
+    This function calculates the genders results
     """
-    print(
-        """\
-                             #        #                   #
- ####                        #       ###                  #
- #   #                       #  #    #                   ###
- ####   # #     ##     ###   # #    ###     ###   ####    #
- #   #  ## #   # ##   #  #   ###     #     #  #   ##      # #
- #   #  #      ##     #  #   #  #    #     #  #     ##    # #
- ####   #       ###    ####  #  #    #      ####  ####     #
+    male_tot = df['gender'].str.contains('Male').sum()
+    female_tot = df['gender'].str.contains('Female').sum()
+    # print(female_tot)
+    male = df[df['gender'] == 'Male']
+    female = df[df['gender'] == 'Female']
+    
+    # calculate question 1
+    male_yes = df[(df['question 1'] == 'Yes') & (df['gender'] == 'Male')]\
+        .value_counts().sum()
+    
+    female_yes = df[(df['question 1'] == 'Yes') & (df['gender'] == 'Female')]\
+        .value_counts().sum()
+
+    female_percent = (female_yes / female_tot) * 100
+    male_percent = (male_yes / male_tot) * 100
 
 
-         ##
-        #  #
-         #     #  #   # #    #  #    ##    #  #
-          #    #  #   ## #   #  #   # ##   #  #
-        #  #   #  #   #       ##    ##     ## #
-         ##     ###   #       ##     ###     ##
-                                           #  #
-                                            ##
-    """
-    )
+# def welcome():
+#     """
+#     Displays a welcome message and introduction to the survey
+#     """
+#     print(
+#         """\
+#                              #        #                   #
+#  ####                        #       ###                  #
+#  #   #                       #  #    #                   ###
+#  ####   # #     ##     ###   # #    ###     ###   ####    #
+#  #   #  ## #   # ##   #  #   ###     #     #  #   ##      # #
+#  #   #  #      ##     #  #   #  #    #     #  #     ##    # #
+#  ####   #       ###    ####  #  #    #      ####  ####     #
 
-    print(
-        """
-        Welcome to the Breakfast Survey.\n
-        This survey analyses breakfast eating habits based on gender and age.\n
-        You can view the results, read a summary or add to the data by
-        completing the survey.
-        """
-    )
+
+#          ##
+#         #  #
+#          #     #  #   # #    #  #    ##    #  #
+#           #    #  #   ## #   #  #   # ##   #  #
+#         #  #   #  #   #       ##    ##     ## #
+#          ##     ###   #       ##     ###     ##
+#                                            #  #
+#                                             ##
+#     """
+#     )
+
+#     print(
+#         """
+#         Welcome to the Breakfast Survey.\n
+#         This survey analyses breakfast eating habits based on gender and age.\n
+#         You can view the results, read a summary or add to the data by
+#         completing the survey.
+#         """
+#     )
 
 
 def main():
     """
     Run all program functions
     """
-    welcome()
+    # welcome()
+    calculate_gender_results()
 
 
 main()
