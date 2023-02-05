cb39e3ca54f57dc0fc860c20f5457181ba09a6c5
orlagh-sweeney <orlagh16@gmail.com>
2023-02-05 13:27:02 +0000

    Create get_total_males and get_total_femamles functions

--- run.py
+++ run.py
@@ -20,18 +20,28 @@ data = SHEET.worksheet('bkdata').get_all_values()
 headers = data.pop(0)
 df = pd.DataFrame(data, columns=headers)
 df.head()
-print(df)
+# print(df)
 
-def calculate_gender_results():
+def get_total_males():
+    """
+    This function calculates the total numbers of males
+    """
+    male_total = df['gender'].str.contains('Male').sum()
+    print(male_total)
+    return male_total
+
+def get_total_females():
+    """
+    This function calculates the total number of females
+    """
+    female_total = df['gender'].str.contains('Female').sum()
+    return female_total
+    print(female_total)
+
+def calculate_gender_results(male_total, female_total):
     """
     This function calculates the genders results
     """
-    male_tot = df['gender'].str.contains('Male').sum()
-    female_tot = df['gender'].str.contains('Female').sum()
-    # print(female_tot)
-    male = df[df['gender'] == 'Male']
-    female = df[df['gender'] == 'Female']
-
     # calculate question 1
     male_yes = df[(df['question 1'] == 'Yes') & (df['gender'] == 'Male')]\
         .value_counts().sum()
@@ -39,8 +49,9 @@ def calculate_gender_results():
     female_yes = df[(df['question 1'] == 'Yes') & (df['gender'] == 'Female')]\
         .value_counts().sum()
 
-    female_percent = (female_yes / female_tot) * 100
-    male_percent = (male_yes / male_tot) * 100
+    female_percent = (female_yes / female_total) * 100
+    print(female_percent)
+    male_percent = (male_yes / male_total) * 100
 
     # calculate question 2
     female_1 = df[(df['question 2'] == '1') & (df['gender'] == 'Male')]\
@@ -78,21 +89,21 @@ def calculate_gender_results():
     male_7 = df[(df['question 2'] == '7') & (df['gender'] == 'Female')]\
         .value_counts().sum()
     
-    female_1_percent = (female_1 / female_tot) * 100
-    female_2_percent = (female_2 / female_tot) * 100
-    female_3_percent = (female_3 / female_tot) * 100
-    female_4_percent = (female_4 / female_tot) * 100
-    female_5_percent = (female_5 / female_tot) * 100
-    female_6_percent = (female_6 / female_tot) * 100
-    female_7_percent = (female_7 / female_tot) * 100
+    # female_1_percent = (female_1 / female_total) * 100
+    # female_2_percent = (female_2 / female_total) * 100
+    # female_3_percent = (female_3 / female_total) * 100
+    # female_4_percent = (female_4 / female_total) * 100
+    # female_5_percent = (female_5 / female_total) * 100
+    # female_6_percent = (female_6 / female_total) * 100
+    # female_7_percent = (female_7 / female_total) * 100
 
-    male_1_percent = (male_1 / male_tot) * 100
-    male_2_percent = (male_2 / male_tot) * 100
-    male_3_percent = (male_3 / male_tot) * 100
-    male_4_percent = (male_4 / male_tot) * 100
-    male_5_percent = (male_5 / male_tot) * 100
-    male_6_percent = (male_6 / male_tot) * 100
-    male_7_percent = (male_7 / male_tot) * 100
+    # male_1_percent = (male_1 / male_total) * 100
+    # male_2_percent = (male_2 / male_total) * 100
+    # male_3_percent = (male_3 / male_total) * 100
+    # male_4_percent = (male_4 / male_total) * 100
+    # male_5_percent = (male_5 / male_total) * 100
+    # male_6_percent = (male_6 / male_total) * 100
+    # male_7_percent = (male_7 / male_total) * 100
 
 
 
@@ -137,7 +148,8 @@ def main():
     Run all program functions
     """
     # welcome()
-    calculate_gender_results()
-
+    male_total = get_total_males()
+    female_total = get_total_females()
+    calculate_gender_results(male_total, female_total)
 
 main()
